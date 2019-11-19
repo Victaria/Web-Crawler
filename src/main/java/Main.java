3c3ed7ab558e14cec86b5d055c2e6b0960de02d4
geflltername <rthem@gmx.de>
2019-11-19 19:19:59 +0100

    add branch library

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String start_url = "https://www.travelontoast.de/";
+
+        Bot bot = new Bot(start_url);
+        bot.start();
+    }
+}
---
+++ src/main/java/site.java
@@ -0,0 +1,14 @@
+public class site {
+    public static void main(String[] args){
+        /*choose site to collect data from extractor
+        //type of sites:
+        Startseite: overview newest posts
+        over this blog: generic
+        Reiseziele->country: categorized posts
+        Reisearten->type: categorized posts
+        Shop:blog:referal link
+        blog:blog:txt,pics,comment field
+         */
+
+    }
+}
