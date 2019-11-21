42daa0f7460963c34fb0e9f498887caa00577853
geflltername <rthem@gmx.de>
2019-11-21 23:31:36 +0100

    rename branch beta -beta is for programming -alpha is for release working versions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //listing all URL`s
         String start_url = "https://www.travelontoast.de/";
 
         Bot bot = new Bot(start_url);
         bot.start();
+
+        //
+
     }
 }
